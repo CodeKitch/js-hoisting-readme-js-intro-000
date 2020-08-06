99c28ca2ba80a0a5a4c4107abecacb2b60eb651d
practical-trainer-5867 <misterjcr@aol.com>
2020-08-06 00:36:57 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -12,15 +12,15 @@ function crazy() {
   // fix the code in here:
 
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+var thisIsCrazy = function (){
+  console.log("hey!!!")
   }
   thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
+var name = "Cricky";
 function sayMy() {
   var name = "Kristin";
   console.log(name);
